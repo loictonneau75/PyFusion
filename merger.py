5234c34c877295a7960eb59f37a6d8abec9c4916
loic <loictonneau75@gmail.com>
2023-08-29 13:51:00 +0200

    creation de l'interface utilisateur

--- merger.py
+++ merger.py
@@ -1,5 +1,9 @@
 import os
-from collections import defaultdict
+import winreg
+import tkinter as tk
+from tkinter import filedialog
+import ttkbootstrap as ttk
+
 
 def get_files_and_path(directory: str, extension: str):
     file_list = []
@@ -17,16 +21,15 @@ def get_files_and_path(directory: str, extension: str):
 def check_words_in_string(word_list: list, input_string: str):
     return all(word not in input_string for word in word_list)
 
-
-def get_text(path_list: list, file_list: list):
+def get_text(path_list: list, file_list: list, output_file_name: str):
     existing_import = []
     main = []
     scripts = []
     for path in path_list:
-        if not path.endswith(output_filename):
+        if not path.endswith(output_file_name):
             with open (path, "r") as file:
                     for line in file:
-                        if line.startswith("import") or line.startswith("from"):
+                        if line.startswith(("import","from")):
                             if line not in existing_import:
                                 if check_words_in_string(file_list, line):
                                     existing_import.append(line)
@@ -38,17 +41,17 @@ def get_text(path_list: list, file_list: list):
     scripts += main
     return existing_import, scripts
 
-def create_test_file(file_path, imports, scripts):
+def create_test_file(file_path: str, imports: str, scripts: str):
     with open(file_path, "w") as output_file:
         output_file.writelines(imports)
         output_file.writelines(scripts)
 
-def create_output_directory(path):
+def create_output_directory(path: str):
     if not os.path.exists(path):
         os.makedirs(path)
 
-def main():
-    directory_path = r"C:\Users\loict\Desktop\pwd_managerV2"
+def execute_script(folder_entry: ttk.Entry, result_label: ttk.Label):
+    directory_path = folder_entry.get()
     output_directory_name = "test"
     output_filename = "test.py"
 
@@ -57,6 +60,46 @@ def main():
     file_list, path_list = get_files_and_path(directory_path, ".py")
     imports, scripts = get_text(path_list, file_list)
     create_test_file(file_path, imports, scripts)
+
+    result_label.config(text="Script exécuté avec succès !")
+
+def is_windows_light_mode():
+    try:
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize")
+        apps_use_light_theme, _ = winreg.QueryValueEx(key, "AppsUseLightTheme")
+        winreg.CloseKey(key)
+        return  apps_use_light_theme
+    
+    except Exception as e:
+        print("Erreur lors de la détection du thème:", e)
+        return None
+
+def main():
+    theme = "flatly" if is_windows_light_mode() else "darkly"
+    root = ttk.Window(themename = theme)
+    root.title("Script Merger")
+    root.config()
+
+    folder_label = ttk.Label(root, text="Choisissez le dossier cible:")
+    folder_entry = ttk.Entry(root)
+    folder_button = ttk.Button(root, text="Parcourir", bootstyle = "secondary", 
+                               command=lambda: folder_entry.insert(tk.END, filedialog.askdirectory()))
+    execute_button = ttk.Button(root, text="Exécuter", bootstyle = "success", 
+                                command = lambda: execute_script(folder_entry,result_label))
+    result_label = ttk.Label(root, text="")
+
+    root.columnconfigure(0, pad = 10)
+    root.columnconfigure(1, pad = 10)
+    root.rowconfigure(0, pad = 10)
+    root.rowconfigure(1, pad = 10)
+
+    folder_label.grid(columnspan = 2, row = 0)
+    folder_entry.grid(column = 0, row = 1)
+    folder_button.grid(column = 1, row = 1)
+    execute_button.grid(columnspan = 2, row = 2)
+    result_label.grid(columnspan = 2, row = 3)
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
