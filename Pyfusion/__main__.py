c2dfb2bc947b8f19a446f5ca3a56486a9490429a
loic <loictonneau75@gmail.com>
2023-09-05 18:14:00 +0200

    ajout de la docstring

--- Pyfusion/__main__.py
+++ Pyfusion/__main__.py
@@ -30,9 +30,26 @@ from typing import Union
 
 from utils import is_os_light_mode
 from script_fusion.script_fusion_app import ScriptFusionApp
-from class_separator.class_separator_app import ClassSeparator
+from class_separator.class_separator_app import ClassSeparatorApp
 
 class PyFusion(ttk.Window):
+    """
+    PyFusion Class for managing a tkinter application window.
+    
+    This class inherits from ttk.Window and sets up a GUI for merging Python scripts
+    or separating Python classes. The user can choose between two functionalities
+    via buttons: ScriptFusion and ClassSeparator. 
+
+    Attributes:
+        merged_button (ttk.Button): Button for ScriptFusion functionality.
+        separator_button (ttk.Button): Button for ClassSeparator functionality.
+
+    Methods:
+        __init__(): Initialize the PyFusion app with a specific theme.
+        create_widgets(): Create GUI widgets for the application.
+        manage_button(app: Union[ScriptFusionApp, ClassSeparatorApp]): Handle button behavior and switch apps.
+        place_widgets(): Position the widgets within the layout pack.
+    """
     def __init__(self):
         """
         Initialize the PyFusion app.
@@ -62,17 +79,27 @@ class PyFusion(ttk.Window):
             None
         """
         self.merged_button = ttk.Button(self,text = "ScritpFusion", command = lambda: self.manage_button(ScriptFusionApp))
-        self.separator_button = ttk.Button(self,text = "ClassSeparator", command = lambda: self.manage_button(ClassSeparator))
+        self.separator_button = ttk.Button(self,text = "ClassSeparator", command = lambda: self.manage_button(ClassSeparatorApp))
         
         self.place_widgets()
 
-    def manage_button(self, app: Union[ScriptFusionApp, ClassSeparator]):
+    def manage_button(self, app: Union[ScriptFusionApp, ClassSeparatorApp]):
+        """
+        Manages the behavior of a button in the application. Destroys the current
+        instance of the class and initializes a new instance of the specified application.
+
+        Args:
+            app (Union[ScriptFusionApp, ClassSeparatorApp]): The application class to initialize.
+
+        Returns:
+            None
+        """
         self.destroy()
         app()
 
     def place_widgets(self):
         """
-        Place widgets within the layout grid.
+        Place widgets within the layout pack.
 
         Args:
             NoneApp
